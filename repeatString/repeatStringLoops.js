81b156c39aa934da98c7d6a7a2d2a0fe0c4ed3b2
ekhamis <ekhamis@gmail.com>
2017-01-14 12:31:46 +0100

    Updated codes

--- repeatString/repeatStringLoops.js
+++ repeatString/repeatStringLoops.js
@@ -1,36 +1,58 @@
 // for loop
 function repeatStringNumTimes(str, num) {
-    result = "";
-    if (num < 0) { return result}
-    for (var i = 0; i < num; i++) {
-        return result = result + str;
+    var result = "";
+    if (num <= 0) {
+        return result;
     }
+    for (var i = 0; i < num; i++) {
+        result = result + str;
+    }
+    return result;
 }
 
-var repeat = repeatStringNumTimes('for loop. ', -3);
+var repeat = repeatStringNumTimes('for loop. ', 3);
 console.log(repeat);
+
 
 // while loop
 function repeatStringNumTimes2(str, num) {
+    var result = "";
     while (num > 0) {
-        return str.repeat(num)
-    } if (num < 0) {
-        return "''";
-    }
+        result = result + str;
+        num--;
+    } return result
 }
-var repeat = repeatStringNumTimes2('while loop. ', 4);
-console.log(repeat);
+var repeat2 = repeatStringNumTimes2('while loop. ', 4);
+console.log(repeat2);
 
 //do loop
-function repeatStringNumTimes3(str, num) {
-    if (num < 0) {
-        return "''";
-    } else {
-        do {
-            return str.repeat(num)
-        } while (num > 0)
-    }
-}
-var repeat = repeatStringNumTimes3('do loop. ', 5);
-console.log(repeat);
 
+function repeatStringNumTimes3(str, num) {    
+    var result = "";
+    do {
+        if (num > 0) {
+            result += str;
+            num--;
+        } else return result;
+
+    } while (num > 0);
+    return result;
+}
+var repeat3 = repeatStringNumTimes3('do loop. ', 3);
+console.log(repeat3);
+
+// do loop 2 recommended by Jim
+function repeatStringNumTimes3(str, num) {
+    if (num <= 0) {
+        return ""
+    }
+
+    var result = "";
+    do {
+        result += str;
+        num--;
+    } while (num > 0);
+    return result;
+}
+var repeat3 = repeatStringNumTimes3('do loop. ', 3);
+console.log(repeat3);
